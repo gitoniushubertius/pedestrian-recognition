55e005e52b90849e6b1380e82670e084e9babc91
Dominik Poljak <dominik.poljak1@gmail.com>
2025-01-18 17:08:29 +0100

    ssd lite and vgg

--- src/dataset.py
+++ src/dataset.py
@@ -3,6 +3,7 @@ from torchvision.transforms import transforms
 import os
 from PIL import Image
 import torch
+from torchvision.io.image import decode_image
 
 
 class CaltechPedestrianDataset(Dataset):
@@ -11,9 +12,10 @@ class CaltechPedestrianDataset(Dataset):
         self.split = split
 
         self.transform = transforms.Compose([
+            # transforms.Resize((320, 320)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+            # transforms.Normalize(mean=[0.485, 0.456, 0.406],
+            #                      std=[0.229, 0.224, 0.225])
         ])
 
         self.examples_dir = os.path.join(self.root_dir, self.split, "examples")
@@ -30,19 +32,27 @@ class CaltechPedestrianDataset(Dataset):
         annotation_path = os.path.join(self.annotations_dir, image_name.replace(".png", ".txt"))
 
         image = Image.open(image_path).convert('RGB')
+        # image = decode_image(image_path)
+        width, height = image.size 
 
         boxes = []
         labels = []
 
+        image = self.transform(image)
+
         with open(annotation_path, "r") as f:
             for line in f:
-                class_id, x_center, y_center, width, height = map(float, line.strip().split())
-                boxes.append([x_center, y_center, width, height])
-                labels.append(int(class_id))
+                class_id, x_center, y_center, box_width, box_height = map(float, line.strip().split())
+                xmin = int((x_center - box_width / 2) * width)
+                ymin = int((y_center - box_height / 2) * height)
+                xmax = int((x_center + box_width / 2) * width)
+                ymax = int((y_center + box_height / 2) * height)
+                boxes.append([xmin, ymin, xmax, ymax])
+                labels.append(1)
 
         boxes = torch.tensor(boxes, dtype=torch.float32)
         labels = torch.tensor(labels, dtype=torch.int64)
 
-        image = self.transform(image)
+        
 
         return image, {'boxes': boxes, 'labels': labels}
